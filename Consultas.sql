9d24121ddd5ce74fe87db8e98500acc3580fd41d
Larissa Gomes <larissagomesgaspar@gmail.com>
2025-11-18 10:55:56 -0300

    Consultas
    Pequena ordenação de como deve ficar escrito, para ficar mais fácil a leitura do código.

--- Consultas.sql
+++ Consultas.sql
@@ -1,5 +1,9 @@
+-- Perguntas de Negócio
+
 -- Quantos estudantes tem empréstimos em atraso?
-SELECT es.Nome AS 'Nome', em.RaEstudante AS 'RA', em.Status AS 'Status'
+SELECT es.Nome AS 'Nome', 
+       em.RaEstudante AS 'RA', 
+       em.Status AS 'Status'
 FROM Estudantes es INNER JOIN Emprestimos em ON em.RaEstudante = es.RA
 WHERE em.Status = 'Atrasado' ORDER BY es.Nome;
 
@@ -12,13 +16,17 @@ INNER JOIN Emprestimos em ON ex.Id = em.IdExemplar
 GROUP BY l.Titulo, l.Categoria ORDER BY COUNT(*) DESC;
 
 -- Quantos exemplares de cada livro estão disponíveis no momento?
-SELECT l.Titulo AS 'Título', ex.Status AS 'Status', COUNT(*) AS 'Quantidade'
+SELECT l.Titulo AS 'Título', 
+       ex.Status AS 'Status', 
+       COUNT(*) AS 'Quantidade'
 FROM Livros l INNER JOIN Exemplares ex ON l.Id = ex.IdLivro
 WHERE ex.Status = 'disponível'
 GROUP BY l.Titulo, ex.Status ORDER BY COUNT(*) ASC;
 
 -- Qual é a taxa de uso dos livros de tecnologia nos últimos 6 meses?
-SELECT l.Titulo AS 'Título', l.Categoria AS 'Categoria', COUNT(*) AS 'Quantidade'
+SELECT l.Titulo AS 'Título', 
+       l.Categoria AS 'Categoria', 
+       COUNT(*) AS 'Quantidade'
 FROM Livros l INNER JOIN Exemplares ex ON l.Id = ex.IdLivro
 INNER JOIN Emprestimos em ON ex.Id = em.IdExemplar
 WHERE em.DataEmprestimo >= DATEADD(MONTH, -6, GETDATE()) AND l.Categoria = 'Tecnologia' -- Aqui pega a data de 6 meses atrás até hoje
